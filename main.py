d95588d8f444bd282a5be317675425ba28966ebc
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-22 16:35:10 +0000

    completed final project

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+##################### Hard Starting Project ######################
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+import pandas
+import datetime as dt
 import random
 import smtplib
-import datetime as dt
 
-my_email="testpython382@gmail.com"
-password="esze crrh jcgx vvdt"
+MY_EMAIL = "testpython382@gmail.com"
+MY_PASSWORD = "esze crrh jcgx vvdt"
 
-now = dt.datetime.now()
-day_of_week = now.weekday()
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
 
-with open("quotes.txt", mode= "r") as file:
-    quotes = file.readlines()
-    quote = random.choice(quotes)
+birthdays = pandas.read_csv("./birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day):data_row for (index, data_row) in birthdays.iterrows()}
 
-if day_of_week == 2:
+if today_tuple in birthdays_dict:
+    birthdays_person = birthdays_dict[today_tuple]
+    random_letter = random.randint(1,3)
+    with open(f"./letter_templates/letter_{random_letter}.txt") as letter:
+        letter_chosen = letter.read()
+        letter_name = letter_chosen.replace("[NAME]", birthdays_person["name"])
+
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user= my_email, password=password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs="bpinheiro44@yahoo.com",
-                            msg=f"Subject:Quote of the week\n\n{quote}")
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthdays_person["email"],
+                            msg=f"Subject:Happy Birthday!\n\n{letter_name}")
